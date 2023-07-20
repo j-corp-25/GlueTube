25547329ac9b1ca374492247fd5ca4eca3b666a0
Jordy <jordycorporan95@gmail.com>
2023-07-20 15:29:49 -0400

    Added seed sample

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ActiveRecord::Base.transaction do
+  User.destroy_all
+
+  ActiveRecord::Base.connection.reset_pk_sequence!("users")
+
+  puts "Creating users..."
+
+  michasel = User.create!(username: "bigPapimario", password: "password")
+  paul = User.create!(username: "imchillin", password: "password")
+  christian = User.create!(username: "PopeyeLifeHacks", password: "password")
+  maggie = User.create!(username: "Mrbeast", password: "password")
+  darwin = User.create!(username: "darude", password: "password")
+  dieseltruck = User.create!(username: "dididexter", password: "password")
+  tylor = User.create!(username: "TheRobinsonFamily", password: "password")
+  dismae = User.create!(username: "dizzzzzznutbar", password: "password")
+end
