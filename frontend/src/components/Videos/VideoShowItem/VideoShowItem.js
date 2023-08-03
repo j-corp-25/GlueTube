9c9feac4c43b86c4ec36829ab20a47e1be31fb9e
Jordy <jordycorporan95@gmail.com>
2023-08-03 05:08:15 -0400

    Centered mini-video

--- frontend/src/components/Videos/VideoShowItem/VideoShowItem.js
+++ frontend/src/components/Videos/VideoShowItem/VideoShowItem.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "../VideoIndex/VideoList.css";
 import "./VideoShowItem.css";
 import { useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 // import videoImg1 from "../../../assets/Video_Image_1.jpeg";
 // import videoImg2 from "../../../assets/Video_Img_2.jpeg";
@@ -9,10 +10,41 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Avatar from "react-avatar";
 import { useSelector } from "react-redux";
 import ReactPlayer from "react-player";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
 function VideoShowItem({ video }) {
-  const videoId = useParams().videoId;
+  // const videoId = useParams().videoId;
   const [isPlaying, setIsPlaying] = useState(false);
   const [isControlsVisible, setIsControlsVisible] = useState(false);
+  const viewCounts = [
+    "20 million views",
+    "10 thousand views",
+    "5 thousand views",
+    "1 million views",
+    "50 million views",
+    "2 million views",
+    "100 thousand views",
+    "500 thousand views",
+    "30 million views",
+    "15 thousand views",
+    "200 thousand views",
+    "3 million views",
+    "40 thousand views",
+    "25 million views",
+    "7 thousand views",
+    "800 thousand views",
+    "4 million views",
+    "60 thousand views",
+    "35 million views",
+    "9 thousand views",
+  ];
+  const [randomViewCount, setRandomViewCount] = useState(null);
+  useEffect(() => {
+    const randomView =
+      viewCounts[Math.floor(Math.random() * viewCounts.length)];
+    setRandomViewCount(randomView);
+  }, []);
+
   let hoverTimeout;
   const handleMouseEnter = () => {
     hoverTimeout = setTimeout(() => {
@@ -92,8 +124,8 @@ function VideoShowItem({ video }) {
           >
             <div className="player-wrapper">
               <ReactPlayer
-                width="340px"
-                height="190px"
+                width="95%"
+                height="100%"
                 className="react-player"
                 controls={isControlsVisible}
                 url={video.videoUrl}
@@ -116,9 +148,15 @@ function VideoShowItem({ video }) {
               ])}
             />
           </div>
-          <div>
+          <div className="video-info">
             <h3 className="video-title">{video.title}</h3>
             <span className="video-author">{author.username}</span>
+            <div className="video-stats">
+              <span className="video-views">{randomViewCount} •</span>
+              <span className="video-created">
+                {formatDistanceToNow(new Date(video.createdAt))} ago
+              </span>
+            </div>
           </div>
         </div>
       </div>
