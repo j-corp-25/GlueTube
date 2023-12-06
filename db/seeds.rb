5332242dec0f0bfd5d9ec070912fe84ed5f045df
Jordy Corporan <95154804+j-corp-25@users.noreply.github.com>
2023-12-06 10:22:19 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,10 @@ require 'faker'
 ActiveRecord::Base.transaction do
   puts "Destroying tables..."
 
-  Comment.destroy_all
-  Video.destroy_all
   User.destroy_all
 
   puts "Resetting primary keys..."
 
-  ActiveRecord::Base.connection.reset_pk_sequence!("comments")
-  ActiveRecord::Base.connection.reset_pk_sequence!("videos")
   ActiveRecord::Base.connection.reset_pk_sequence!("users")
 
   puts "Creating demouser"
@@ -23,52 +19,6 @@ ActiveRecord::Base.transaction do
     first_name: 'demo',
     last_name: 'user'
   )
-
-  puts "Creating video for demouser..."
-
-  demo_video = Video.create!({
-    title: Faker::Book.title,
-    description: Faker::Lorem.paragraph,
-    author_id: demo_user.id
-  })
-
-  demo_video.video.attach(
-    io: URI.parse("https://my-gluetube-seeds.s3.amazonaws.com/video_1.mp4").open,
-    filename: "video_1.mp4"
-  )
-
-  # Create additional users with videos and comments
-  5.times do |i|
-    user = User.create!({
-      username: Faker::Internet.unique.username(specifier: 3),
-      email: Faker::Internet.unique.email,
-      password: 'password',
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name
-    })
-
-    puts "Creating video for #{user.username}..."
-
-    video = Video.create!({
-      title: Faker::Book.title,
-      description: Faker::Lorem.paragraph,
-      author_id: user.id
-    })
-
-    video.video.attach(
-      io: URI.parse("https://my-gluetube-seeds.s3.amazonaws.com/video_#{i + 2}.mp4").open,
-      filename: "video_#{i + 2}.mp4"
-    )
-
-    # Create random comments for each video
-    rand(5..10).times do
-      Comment.create!(
-        body: Faker::Lorem.sentence,
-        author_id: User.all.sample.id,
-        video_id: video.id
-      )
-    end
-  end
 
   puts "Seed data created successfully!"
 end
